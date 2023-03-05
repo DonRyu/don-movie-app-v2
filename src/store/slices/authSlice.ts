22c7a7e79b50ccf69ae264cd3720f68f0cefdbbc
DonRyu <alpharyu1031@gmail.com>
2023-03-04 16:44:50 -0800

    edit

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -1,22 +1,22 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import {Cookies} from 'react-cookie'
-const cookies = new Cookies()
+import { Cookies } from 'react-cookie';
+const cookies = new Cookies();
 
-
+const login = createAsyncThunk('auth/login', async () => {
+  const resp = await fetch('https://api.countapi.xyz/hit/opesaljkdfslkjfsadf.com/visits');
+  const data = await resp.json();
+  return data.value;
+});
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState:{
-    isLogin:false
+  initialState: {
+    isLogin: false,
   },
   reducers: {
-    isLogin:(state, action)=>{
-       
-    }
+    isLogin: (state, action) => {},
   },
-  extraReducers: (builder) => {
-   
-  },
+  extraReducers: (builder) => {},
 });
 
 export default authSlice.reducer;
